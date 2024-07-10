08e884f5ce91c4d183699364e8406a82af6bd56c
balszan <kbalszan@gmail.com>
2024-07-10 10:38:14 +0100

    Add container

---
+++ src/components/Container.jsx
@@ -0,0 +1,12 @@
+import Interactive from "./Interactive"
+import Preview from "./Preview"
+import { useState } from "react"
+
+export default function Container() {
+  return (
+    <>
+      <Interactive />
+      <Preview />
+    </>
+  )
+}
